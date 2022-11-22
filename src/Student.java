4713a10001276ce4f0a83f74945cd7e5088a5bad
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-22 08:35:48 +0530

    add student added

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    int admNumber;
+    String name;
+    int rollNumber;
+    String collegeName;
+    long phoneNumber;
+    String email;
+    public void addEmployee(int admNumber,String name,int rollNumber,String collegeName,Long phoneNumber,String emailAddress){
+        this.admNumber=admNumber;
+        this.name=name;
+        this.rollNumber=rollNumber;
+        this.collegeName=collegeName;
+        this.phoneNumber=phoneNumber;
+        this.email=emailAddress;
+    }
+}
